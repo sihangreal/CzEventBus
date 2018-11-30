589b5030c735806d9d62ff96f7a116b370fc760a
陈思行 <chensh@legion-tech.net>
2018-11-30 18:11:13 +0800

    初始化

---
+++ EventBusTest/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CzEventBus;
+
+namespace EventBusTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //EventBus.PublishEvent("EventTest");
+
+           // EventBus.PublishEvent("EventTest",  "hello C#" );
+
+            //EventBus.PublishEvent("EventTest",this, "hello C#");
+
+            EventBus.PublishEvent("EventTest",this,new object[] {"hello C++"});
+        }
+    }
+}
---
+++ EventBusTest/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CzEventBus;
+
+namespace EventBusTest
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            EventBus.RegisterEvent(this);
+        }
+
+        //[EventAttr("EventTest")]
+        //public void EventTest()
+        //{
+        //    this.label1.Text = "hello world!";
+        //}
+
+        //[EventAttr("EventTest")]
+        //public void EventTest(string msg)
+        //{
+        //    this.label1.Text = msg;
+        //}
+
+
+        [EventAttr("EventTest")]
+        public void EventTest(object sender, string msg)
+        {
+            this.label1.Text = msg;
+        }
+    }
+}
