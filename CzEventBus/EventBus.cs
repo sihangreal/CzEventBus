a368bcd869a9f6ed4b2dd04fc881c3ad5a6ebccd
sihangreal <33060987+sihangreal@users.noreply.github.com>
2019-09-04 14:15:33 +0800

    Update EventBus.cs
    优化，利用多线程执行方法，达到异步执行的效果

--- CzEventBus/EventBus.cs
+++ CzEventBus/EventBus.cs
@@ -1,9 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CzEventBus
 {
@@ -14,7 +17,45 @@ namespace CzEventBus
             public object listener;
             public MethodInfo methodInfo;
         }
+
+        private class InvokeParam
+        {
+            public ListenerInfo listener;
+            public object[] param;
+        }
+
+        //存放事件的容器
         private static Dictionary<string, List<ListenerInfo>> listenerMap = new Dictionary<string, List<ListenerInfo>>();
+
+        ///// <summary>
+        ///// 注册事件 最懒的方式不推荐使用
+        ///// </summary>
+        ///// <param name="assembly">程序集</param>
+        //public static void Register(Assembly assembly, bool isFormOrControl = false)
+        //{
+        //    var types = assembly.ExportedTypes;
+        //    foreach (var type in types)
+        //    {
+        //        if (isFormOrControl)
+        //        {
+        //            if (typeof(Form).IsAssignableFrom(type) || typeof(Control).IsAssignableFrom(type))
+        //            {
+        //                object instance = Activator.CreateInstance(type);
+        //                RegisterEvent(instance);
+        //            }
+        //        }
+        //        else
+        //        {
+        //            object instance = Activator.CreateInstance(type);
+        //            RegisterEvent(instance);
+        //        }
+        //    }
+        //}
+
+        /// <summary>
+        /// 注册事件
+        /// </summary>
+        /// <param name="listener">监听者</param>
         public static void RegisterEvent(object listener)
         {
             Type type = listener.GetType();
@@ -33,6 +74,12 @@ namespace CzEventBus
                 }
             }
         }
+
+        /// <summary>
+        /// 注册事件
+        /// </summary>
+        /// <param name="eventType">事件名</param>
+        /// <param name="listener">监听者</param>
         public static void RegisterEvent(string eventType, object listener)
         {
             Type type = listener.GetType();
@@ -47,6 +94,13 @@ namespace CzEventBus
                 RegisterEvent(eventType, methodInfo, listener);
             }
         }
+
+        /// <summary>
+        /// 注册事件
+        /// </summary>
+        /// <param name="eventType">事件名</param>
+        /// <param name="method">方法</param>
+        /// <param name="listener">监听者</param>
         public static void RegisterEvent(string eventType, MethodInfo method, object listener)
         {
             if (IsRegisterEvent(eventType, method, listener))
@@ -68,6 +122,14 @@ namespace CzEventBus
             listenerInfo.listener = listener;
             listenInfos.Add(listenerInfo);
         }
+
+        /// <summary>
+        ///是否注册事件
+        /// </summary>
+        /// <param name="eventType">事件名</param>
+        /// <param name="method">方法</param>
+        /// <param name="listener">监听者</param>
+        /// <returns></returns>
         public static bool IsRegisterEvent(string eventType, MethodInfo method, object listener)
         {
             if (!listenerMap.ContainsKey(eventType))
@@ -84,6 +146,11 @@ namespace CzEventBus
             }
             return false;
         }
+
+        /// <summary>
+        /// 注销事件
+        /// </summary>
+        /// <param name="listener"></param>
         public static void UnRegisterEvent(object listener)
         {
             Type type = listener.GetType();
@@ -98,10 +165,23 @@ namespace CzEventBus
                 UnRegisterEvent(eventAttr.EventType, methodInfo, listener);
             }
         }
+
+        /// <summary>
+        /// 注销事件
+        /// </summary>
+        /// <param name="eventType">事件名</param>
+        /// <param name="listener">监听者</param>
         public static void UnRegisterEvent(string eventType, object listener)
         {
             UnRegisterEvent(eventType, null, listener);
         }
+
+        /// <summary>
+        /// 注销事件
+        /// </summary>
+        /// <param name="eventType">事件名</param>
+        /// <param name="methodInfo">方法</param>
+        /// <param name="listener">监听者</param>
         public static void UnRegisterEvent(string eventType, MethodInfo methodInfo, object listener)
         {
             if (!listenerMap.ContainsKey(eventType))
@@ -131,11 +211,24 @@ namespace CzEventBus
                 }
             }
         }
-        public static void PublishEvent(string eventType)
+
+        /// <summary>
+        /// 发布事件
+        /// </summary>
+        /// <param name="eventType">事件名</param>
+        /// <param name="isAsync">是否支持异步</param>
+        public static void PublishEvent(string eventType, bool isAsync = false)
         {
-            PublishEvent(eventType, null, null);
+            PublishEvent(eventType, null, null, isAsync);
         }
-        public static void PublishEvent(string eventType, object eventArg)
+
+        /// <summary>
+        /// 发布事件
+        /// </summary>
+        /// <param name="eventType">事件名</param>
+        /// <param name="eventArg">事件参数</param>
+        /// <param name="isAsync">是否支持异步</param>
+        public static void PublishEvent(string eventType, object eventArg, bool isAsync = false)
         {
             if (!listenerMap.ContainsKey(eventType))
             {
@@ -149,10 +242,26 @@ namespace CzEventBus
                 {
                     objs = new object[] { eventArg };
                 }
-                listenerInfo.methodInfo.Invoke(listenerInfo.listener, objs);
+                if (isAsync)
+                {
+                    InvokeParam invokeParam = new InvokeParam() { listener = listenerInfo, param = objs };
+                    ThreadPool.QueueUserWorkItem(Invoke, invokeParam);
+                }
+                else
+                {
+                    listenerInfo.methodInfo.Invoke(listenerInfo.listener, objs);
+                }
             }
         }
-        public static void PublishEvent(string eventType, object sender, object eventArg)
+
+        /// <summary>
+        /// 发布消息
+        /// </summary>
+        /// <param name="eventType">事件名</param>
+        /// <param name="sender">发送者</param>
+        /// <param name="eventArg">事件参数</param>
+        /// <param name="isAsync">是否支持异步</param>
+        public static void PublishEvent(string eventType, object sender, object eventArg, bool isAsync = false)
         {
             if (!listenerMap.ContainsKey(eventType))
             {
@@ -166,11 +275,25 @@ namespace CzEventBus
                 {
                     objs = new object[] { sender, eventArg };
                 }
-                listenerInfo.methodInfo.Invoke(listenerInfo.listener, objs);
+                if (isAsync)
+                {
+                    InvokeParam invokeParam = new InvokeParam() { listener = listenerInfo, param = objs };
+                    ThreadPool.QueueUserWorkItem(Invoke, invokeParam);
+                }
+                else
+                {
+                    listenerInfo.methodInfo.Invoke(listenerInfo.listener, objs);
+                }
             }
         }
 
-        public static void PublishEvent(string eventType,object[] eventArgs)
+        /// <summary>
+        /// 发布事件
+        /// </summary>
+        /// <param name="eventType">事件名</param>
+        /// <param name="eventArgs">事件参数</param>
+        /// <param name="isAsync">是否支持异步</param>
+        public static void PublishEvent(string eventType, object[] eventArgs, bool isAsync = false)
         {
             if (!listenerMap.ContainsKey(eventType))
             {
@@ -186,11 +309,26 @@ namespace CzEventBus
                     objList.AddRange(eventArgs);
                     objs = objList.ToArray();
                 }
-                listenerInfo.methodInfo.Invoke(listenerInfo.listener, objs);
+                if (isAsync)
+                {
+                    InvokeParam invokeParam = new InvokeParam() { listener = listenerInfo, param = objs };
+                    ThreadPool.QueueUserWorkItem(Invoke, invokeParam);
+                }
+                else
+                {
+                    listenerInfo.methodInfo.Invoke(listenerInfo.listener, objs);
+                }
             }
         }
 
-        public static void PublishEvent(string eventType,object sender,object[] eventArgs)
+        /// <summary>
+        /// 发布事件
+        /// </summary>
+        /// <param name="eventType">事件名</param>
+        /// <param name="sender">发送者</param>
+        /// <param name="eventArgs">事件参数</param>
+        /// <param name="isAsync">是否支持异步</param>
+        public static void PublishEvent(string eventType, object sender, object[] eventArgs, bool isAsync = false)
         {
             if (!listenerMap.ContainsKey(eventType))
             {
@@ -207,8 +345,26 @@ namespace CzEventBus
                     objList.AddRange(eventArgs);
                     objs = objList.ToArray();
                 }
-                listenerInfo.methodInfo.Invoke(listenerInfo.listener, objs);
+                if (isAsync)
+                {
+                    InvokeParam invokeParam = new InvokeParam() { listener = listenerInfo, param = objs };
+                    ThreadPool.QueueUserWorkItem(Invoke, invokeParam);
+                }
+                else
+                {
+                    listenerInfo.methodInfo.Invoke(listenerInfo.listener, objs);
+                }
             }
+        }
+
+        /// <summary>
+        /// 利用线程池执行方法
+        /// </summary>
+        /// <param name="arg"></param>
+        private static void Invoke(object arg)
+        {
+            InvokeParam invokeParam = (InvokeParam)arg;
+            invokeParam.listener.methodInfo.Invoke(invokeParam.listener.listener, invokeParam.param);
         }
     }
 }
